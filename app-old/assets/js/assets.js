75dc536e0881ff895a732a09039d6b3f0c9f0c6d
unknown <bramdemmer@gmail.com>
2019-09-03 15:35:52 +0200

    remove old app

--- app-old/assets/js/assets.js
+++
@@ -1,6 +0,0 @@
-/* This will load all files within a specific folder */
-
-require.context('../icons/', true, /\.svg$/);
-require.context('../images/', true, /\.(png|svg|jp(e*)g|gif)$/);
-// require.context('../fonts/', true, /\.(woff|woff2|eot|ttf|otf)$/);
-// require.context('../favicons/', true, /\.(svg|png|ico|xml|json|webmanifest)$/);
